11e567a54cab3044a9839cf5c88214c2624740fd
Vanessa Law <vanessalaw95@gmail.com>
2020-05-28 22:48:27 +0800

    First Commit

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField(auto_now=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
